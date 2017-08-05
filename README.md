9e79ec8ebad6290d49b667bdcfd8a984c07f1911
Gabe Rubio <gtrubio192@gmail.com>
2017-08-04 19:38:08 -0600

    initializing react app

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# React Bookmark App
+
+
+## To use
+
+Start a server with one of the following:
+
+### Node
+
+```sh
+npm install
+node server.js
+```
+
+And visit <http://localhost:3000/>.
+
+## Changing the port
+
+You can change the port number by setting the `$PORT` environment variable before invoking any of the scripts above, e.g.,
+
+```sh
+PORT=3001 node server.js
+```
---
+++ public/scripts/example.js
@@ -0,0 +1,196 @@
+
+var ListItem = React.createClass({
+  rawMarkup: function() {
+    var md = new Remarkable();
+    var rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+  },
+
+  render: function() {
+    return (
+      <div className="listing col-sm-10" data-key={this.props.id}>
+        <span className="listing-title">
+          {this.props.title}
+        </span>
+        <a className="listing-url" target="_blank" href={this.props.children} dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
+
+var BookmarkApp = React.createClass({
+  loadListFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'GET',
+      cache: true,
+      headers: { 'Authorization': '476327c41dca6fba27c0752040076b19'},
+      success: function(response) {       
+        this.setState({data: response.data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleListSubmit: function(attributes) {
+    var listItem = {attributes};  
+    var postData = {
+      "data": {
+          "attributes": {
+            "title": listItem.attributes.title,
+            "url": listItem.attributes.url
+          }
+        }
+    };
+
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: postData,
+      headers: { 'Authorization': '476327c41dca6fba27c0752040076b19'},
+      success: function(data) {
+        var bookmarks = this.state.data;
+        var newBookmarks = bookmarks.concat([listItem]);
+        this.setState({data: newBookmarks});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        // trigger modal to alert user their post was unsuccessful
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  handleDelete: function(data) {
+    $.ajax({
+      url: `${this.props.url}/${data}`,  
+      dataType: 'json',
+      type: 'DELETE',
+      headers: { 'Authorization': '476327c41dca6fba27c0752040076b19'},
+      success: function() {
+
+      }.bind(this),
+      error: function(xhr, status, err) {
+        // trigger modal to alert user their post was unsuccessful
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+
+    var updatedList = this.state.data;
+    var index = updatedList.findIndex((listItem) => {
+      return listItem.id === data;
+    })
+    updatedList.splice(index, 1);
+    this.setState({data: updatedList});
+    console.log(this.state.data);  
+  },
+
+  getInitialState: function() {
+    return { data: [] };
+  },
+  componentDidMount: function() {
+    this.loadListFromServer();
+    setInterval(this.loadListFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      <div className="listing-app">
+        <span className="header-title">My Bookmarks</span>
+        <ListForm onListSubmit={this.handleListSubmit} />
+        <BookmarkList list={this.state.data} onDelete={this.handleDelete} onEdit={this.handleEdit} />
+      </div>
+    );
+  }
+});
+ 
+var BookmarkList = React.createClass({
+
+  deleteListing: function(e) {
+    console.log(e);
+    var $itemToDelete = $(e.target.parentElement).data() || $(e.target).data() ;
+    this.props.onDelete($itemToDelete.id);
+  },
+
+  editListing: function(e) {
+    this.props.onEdit('blank data');
+  },
+  
+  render: function() {
+    var listNodes = this.props.list.map((bookmark) => {
+      return (
+        <div className="col-xs-12 listing-box">
+          <ListItem title={bookmark.attributes.title} id={bookmark.id} >
+            {bookmark.attributes.url} 
+          </ListItem>
+          <span className="js-delete listing-control" type="button" onClick={this.deleteListing} data-id={bookmark.id} >
+            <span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
+          </span>
+          <span className="js-edit listing-control" type="button" onClick={this.editListing} data-id={bookmark.id} >
+            <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+          </span>
+
+        </div>
+      );
+    });
+    return (
+      <div>
+         {listNodes} 
+      </div>
+    );
+  }
+});
+
+var ListForm = React.createClass({
+  getInitialState: function() {
+    return {title: '', url: ''};
+  },
+  handleTitleChange: function(e) {
+    this.setState({title: e.target.value});
+  },
+  handleUrlChange: function(e) {
+    this.setState({url: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var title = this.state.title.trim();
+    var url = this.state.url.trim();
+    if (!title || !url) {
+      return;
+    }
+    this.props.onListSubmit({title: title, url: url});
+    this.setState({title: '', url: ''});
+  },
+  render: function() {
+    return (
+      <form className="listing-form form-inline" onSubmit={this.handleSubmit}>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            maxLength="30"
+            placeholder="Site Title"
+            value={this.state.title}
+            onChange={this.handleTitleChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Site URL"
+            value={this.state.url}
+            onChange={this.handleUrlChange}
+          />
+        </div>
+        <input className="btn btn-primary" type="submit" value="Enter" />
+      </form>
+    );
+  }
+});
+
+ReactDOM.render(
+  <BookmarkApp url="http://clientside-api.herokuapp.com/api/v1/listings" pollInterval={10000} />,
+  document.getElementById('content')
+);
